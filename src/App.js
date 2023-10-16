5d995835e979d0f8e63a3aba8756f57a9aa4f1a6
Isa Carvalho <isadecarvalhodev@gmail.com>
2023-10-16 22:14:21 +0100

    add board

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-
+import React, {useState} from "react";
 import './App.css';
+import {Board} from "./components/Board"
 
-import {Box} from "./components/Box"
+
 
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const handBoxClick = (box)
+
+
   return (
     <div className="App">
-  
-  <Box value="X" onClick={null}/> 
+      <Board board={board} onClick={null} /> 
 
     </div>
   );
